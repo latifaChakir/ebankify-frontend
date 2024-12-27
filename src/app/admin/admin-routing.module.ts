8ae7f9337a6b3cc0e64783be30c015f1fd2589f2
latifaChakir <chakirlatifa2001@gmail.com>
2024-12-27 12:34:00 +0100

    add users pages

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UserManagementComponent} from "./user-management/user-management.component";
+import {sessionGuard} from "../core/guards/session.guard";
 
 const routes: Routes = [
-  {path : "users" , component :UserManagementComponent },
+  {path : "users" , component :UserManagementComponent,canActivate: [sessionGuard],
+    //data: { role: 'ADMIN' }
+  },
 ];
 
 @NgModule({
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import {UserManagementComponent} from "./user-management/user-management.component";
+import {NavComponent} from "../layouts/nav/nav.component";
+import {SideComponent} from "../layouts/side/side.component";
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import {UserManagementComponent} from "./user-management/user-management.compone
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    NavComponent,
+    SideComponent
   ]
 })
 export class AdminModule { }
